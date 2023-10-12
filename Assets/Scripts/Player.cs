83f489d957389c0f32447c8b013ec7401a730c8e
LordPelmeha <sashasofolov@gmail.com>
2023-10-12 23:02:32 +0300

    Added rotation and Arrow sprite
    Player follows the cursor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,19 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+
         //Передеть направление
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        moveDelta = new Vector3(x, y, 0);
-        //Отзеракливание спрайта
-        if (moveDelta.x > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+
+        //Поворот игрока за мышкой
+        Vector3 d = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float z = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
+        Debug.Log(z);
+        transform.rotation = Quaternion.Euler(0, 0, z);
+
         //Перемещение игрока
+        moveDelta = new Vector3(x, y, 0);
         transform.Translate(moveDelta * Time.deltaTime);
     }
 }

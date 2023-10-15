c1f1c38e01784b8fcc5f9e2d3e1cc771d68b3862
LordPelmeha <sashasofolov@gmail.com>
2023-10-15 17:54:06 +0300

    Strated work with masks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    public Image Masks;
+    public Sprite fireMask;
+    public Sprite waterMask;
+    public Sprite airMask;
+    public Sprite earthMask;
+
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -26,5 +33,15 @@ public class Player : MonoBehaviour
         Vector3 d = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float z = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, z);
+        //
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Masks.sprite = fireMask;
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            Masks.sprite = waterMask;
+       else if (Input.GetKeyDown(KeyCode.Alpha3))
+            Masks.sprite = airMask;
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+            Masks.sprite = earthMask;
+
     }
 }

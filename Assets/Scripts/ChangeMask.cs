645e862a1b0c71478fb72653a2f007ce7c42fa8b
LordPelmeha <sashasofolov@gmail.com>
2023-10-16 22:53:41 +0300

    Updated ChangeMask

---
+++ Assets/Scripts/ChangeMask.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMask : MonoBehaviour
+{
+    public Image Masks;
+    public Sprite fireMask;
+    public Sprite waterMask;
+    public Sprite earthMask;
+    public Sprite airMask;
+    private int scrollMask = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Изменение счётчика масок за счёт прокурутки колёсика
+        if (Input.mouseScrollDelta.y > 0)
+            scrollMask = (scrollMask + 1) % 4;
+        else if (Input.mouseScrollDelta.y < 0)
+        {
+            if (scrollMask == 0)
+                scrollMask = 4;
+            scrollMask = (scrollMask - 1) % 4;
+        }
+
+        //Меняет спрайт маски в зависимости от нажатой клавиши
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Masks.sprite = fireMask;
+            scrollMask = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Masks.sprite = waterMask;
+            scrollMask = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Masks.sprite = earthMask;
+            scrollMask = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Masks.sprite = airMask;
+            scrollMask = 3;
+        }
+        //Меняет спрайт в зависимости от счётчика прокрутки мыши
+        else if (scrollMask == 0)
+            Masks.sprite = fireMask;
+        else if (scrollMask == 1)
+            Masks.sprite = waterMask;
+        else if (scrollMask == 2)
+            Masks.sprite = earthMask;
+        else if (scrollMask == 3)
+            Masks.sprite = airMask;
+
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,7 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
-    public Image Masks;
-    public Sprite fireMask;
-    public Sprite waterMask;
-    public Sprite airMask;
-    public Sprite earthMask;
+
 
     private void Start()
     {
@@ -32,16 +28,6 @@ public class Player : MonoBehaviour
         //Поворот игрока за мышкой
         Vector3 d = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float z = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, z);
-        //
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-            Masks.sprite = fireMask;
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-            Masks.sprite = waterMask;
-       else if (Input.GetKeyDown(KeyCode.Alpha3))
-            Masks.sprite = airMask;
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-            Masks.sprite = earthMask;
-
+        transform.rotation = Quaternion.Euler(0, 0, z - 90);
     }
 }

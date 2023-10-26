5f83ae87478ac38c0cc2d0b3e853c2f28d62c18e
LordPelmeha <sashasofolov@gmail.com>
2023-10-26 18:21:17 +0300

    changed the display of sprites

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,10 +24,10 @@ public class Player : MonoBehaviour
         moveDelta = new Vector3(x, y, 0);
         transform.rotation = Quaternion.Euler(0, 0, 0); //фиксит баг с поворотом. Можно будет попробовать переделать
         //Коллизия
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector3(0, moveDelta.y,0), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Wall"));
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector3(0, moveDelta.y, 0), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Wall"));
         if (hit.collider == null)
             transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector3(moveDelta.x, 0,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Wall"));
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector3(moveDelta.x, 0, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Wall"));
         if (hit.collider == null)
             transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
         //transform.Translate(moveDelta * Time.deltaTime);

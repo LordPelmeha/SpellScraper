ba23d27b8a7cfe021769612d93fc2fe5b7b54bac
Yamat0Damashii <112909683+Yamat0Damashii@users.noreply.github.com>
2024-04-22 18:38:17 +0300

    AMiniboss base added, patrolling enemy improved

--- Assets/Scripts/Enemy/EnemyMagicHand.cs
+++ Assets/Scripts/Enemy/EnemyMagicHand.cs
@@ -8,30 +8,30 @@ using Unity.VisualScripting;
 
 public class EnemyMagicHand : MonoBehaviour
 {
-    [SerializeField] Enemy en;
-    [SerializeField] Transform player;
-    [SerializeField] private Transform shotPoint;
+    [SerializeField] protected Enemy Owner;
+    [SerializeField] protected Transform player;
+    [SerializeField] protected Transform shotPoint;
     public GameObject Bullet;
 
     public float offset;
-    private float z;
-    private float TimeBtwShots;
+    protected float z;
+    protected float TimeBtwShots;
     public float StartTimeBtwShots;
 
-    private void Start()
+    protected void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
 
-    private void Update()
+    protected virtual void Update()
     {
         Vector3 d = player.transform.position - transform.position;
         z = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, z + offset);
 
-        if (Vector3.Distance(en.transform.position, player.position) < en.detectionRange && CanSeePlayer())
+        if (Vector3.Distance(Owner.transform.position, player.position) < Owner.detectionRange && CanSeePlayer())
         {
 
             if (TimeBtwShots <= 0)
@@ -45,14 +45,14 @@ public class EnemyMagicHand : MonoBehaviour
         }
     }
 
-    void InstantiateWithRotation(GameObject bulletPrefab)
+    protected void InstantiateWithRotation(GameObject bulletPrefab)
     {
         //Quaternion bulletRotation = Quaternion.Euler(0, 0, angle - 90f);
         Instantiate(bulletPrefab, shotPoint.position, shotPoint.rotation);
     }
     protected bool CanSeePlayer()
     {
-        //RaycastHit2D hit = Physics2D.Raycast(transform.position, player.position);
+
         RaycastHit2D hit = Physics2D.Linecast(transform.position, player.position, 1 << LayerMask.NameToLayer("Action"));
 
         if (hit.collider != null)

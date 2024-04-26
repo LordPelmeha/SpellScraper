ff59beb20865a9d621195e8d78de1be9d399a3bc
LordPelmeha <sashasofolov@gmail.com>
2024-04-26 16:15:44 +0300

    fix resolution and volume
    removed duplicate resolutions, the music volume is maintained when switching from level to level

--- Assets/Scripts/Menu/Settings.cs
+++ Assets/Scripts/Menu/Settings.cs
@@ -5,20 +5,22 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 using System;
+using System.Linq;
 
 public class Settings : MonoBehaviour
 {
-    public TMP_Dropdown resolutionDropdown;
+    [SerializeField] protected TMP_Dropdown resolutionDropdown;
 
     Resolution[] resolutions;
-    public AudioSource audioSrc;
+    [SerializeField] protected AudioSource audioSrc;
+    private int currentResolutionIndex;
     void Start()
     {
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         resolutions = Screen.resolutions;
 
-        int currentResolutionIndex = 0;
+        currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
@@ -26,9 +28,10 @@ public class Settings : MonoBehaviour
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
                 currentResolutionIndex = i;
         }
+        resolutions = resolutions.Distinct().ToArray();
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.RefreshShownValue();
-        LoadSettings(currentResolutionIndex);
+        LoadSettings();
     }
     public void SetMusicVolume(float volume)
     {
@@ -62,10 +65,8 @@ public class Settings : MonoBehaviour
         PlayerPrefs.SetFloat("MusicVolumePreference", audioSrc.volume);
         PlayerPrefs.Save();
     }
-    public void LoadSettings(int currentResolutionIndex)
+    public void LoadSettings()
     {
-
-
         if (PlayerPrefs.HasKey("ResolutionPreference"))
             resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
         else
@@ -79,6 +80,5 @@ public class Settings : MonoBehaviour
             audioSrc.volume = PlayerPrefs.GetFloat("MusicVolumePreference");
             Console.WriteLine("fafsa");
         }
-            
     }
 }

7f79d4b915889c01d5e241fd870686e52a4ab259
Yamat0Damashii <112909683+Yamat0Damashii@users.noreply.github.com>
2024-04-30 02:18:40 +0300

    minor improvements

--- Assets/Scripts/Level/DestructibleObj.cs
+++ Assets/Scripts/Level/DestructibleObj.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class DestructibleObj : MonoBehaviour
 {
     [SerializeField] int health;
+    private List<GameObject> miniBossWalls;
+    private List<GameObject> bossWalls;
+    public void Start()
+    {
+        miniBossWalls = GetComponent<MiniBoss>().walls;
+        bossWalls = GetComponent<FinalBoss>().walls;
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Projectile"))
             health--;
         if (health <= 0)
+        {
             Destroy(gameObject);
+            miniBossWalls.Remove(gameObject);
+            bossWalls.Remove(gameObject);
+        }
+            
     }
+
 }

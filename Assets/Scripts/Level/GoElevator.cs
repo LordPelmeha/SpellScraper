c966db874f65bb8da3c5c6c2fcac46f5e3f3d9e3
LordPelmeha <sashasofolov@gmail.com>
2024-05-19 18:53:54 +0300

    changed pauseMenu and goElevator

--- Assets/Scripts/Level/GoElevator.cs
+++ Assets/Scripts/Level/GoElevator.cs
@@ -8,18 +8,23 @@ using UnityEngine.UI;
 public class GoElevator : MonoBehaviour
 {
     public FadeScreen fadeScreen;
+    private string prev;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             PlayerPrefs.SetString("PreviousScene", SceneManager.GetActiveScene().name);
+            prev = SceneManager.GetActiveScene().name;
             StartCoroutine(FadeAndLoadScene());
         }
-            
+
     }
     private IEnumerator FadeAndLoadScene()
     {
         yield return StartCoroutine(fadeScreen.FadeIn());
-        SceneManager.LoadScene("Elevator");
+        if (prev.Contains("Level"))
+            SceneManager.LoadScene($"Boss{int.Parse(prev[^1].ToString())}");
+        if (prev.Contains("Boss"))
+            SceneManager.LoadScene("Elevator");
     }
 }

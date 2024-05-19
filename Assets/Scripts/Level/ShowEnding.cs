a1be39e55a6508d369b704799bb85b282b6b6727
LordPelmeha <sashasofolov@gmail.com>
2024-05-20 00:29:02 +0300

    сссс

--- Assets/Scripts/Level/ShowEnding.cs
+++ Assets/Scripts/Level/ShowEnding.cs
@@ -6,13 +6,23 @@ using UnityEngine;
 
 public class ShowEnding : ChangeMask
 {
-    public TextMeshProUGUI MyText;
-    private string ending;
+    public Image ending;
+
+    public Sprite end1;
+    public Sprite end2;
+
+    //public TextMeshProUGUI MyText;
+    //private string ending;
+
 
      private void Start()
     {
-        ending = PlayerPrefs.GetInt("CountEnd") >= 0 ? "хорошую" : "плохую";
-        MyText.text = $"Поздравляем! Вы прошли игру на {ending} концовку. Дальше будет больше.\nСпасибо, что играли!";
+        //ending = PlayerPrefs.GetInt("CountEnd") >= 0 ? "хорошую" : "плохую";
+        //MyText.text = $"Поздравляем! Вы прошли игру на {ending} концовку. Дальше будет больше.\nСпасибо, что играли!";
+        if (PlayerPrefs.GetInt("CountEnd") >= 0)
+            ending.sprite = end1;
+        else
+            ending.sprite = end2;
     }
     void Update()
     {
